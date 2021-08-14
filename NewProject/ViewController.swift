b86729a696664aa1d9e0e0f26bcde2ce15204674
khushiduddi <kduddi2@illinois.edu>
2021-08-13 17:00:42 -0700

    Initial Commit

---
+++ NewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Khushi Duddi on 8/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
